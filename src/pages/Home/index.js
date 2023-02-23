62e48bb6000b0ad461938b13874b73031fb92154
jlmsdev <joaosouza1135@gmail.com>
2023-02-22 21:28:32 -0300

    Finalizando Banner Pagina Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,18 @@
+import './home.css';
+
+import bannerHome from '../../assets/banner-home.png';
+
 
 export default function Home() {
     return(
-        <>
-            
-        </>
+        <section className="bannerHome">
+
+            <figure>
+                <img className='imgHome' src={ bannerHome } alt="Imagem de Sorvete na Home Page" />
+
+                <figcaption className='legendaImg'>SORVETE ARTESANAL</figcaption>
+            </figure>
+
+        </section>
     );
 }
